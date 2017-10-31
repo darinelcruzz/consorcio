67543cb4f4284a7e8b0fef7468c552f2e1f95f97
Darinel <darinelcruzz@gmail.com>
2017-10-30 20:07:59 -0600

    Login screen changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,51 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+@section('htmlheader')
+    @include('adminlte::layouts.partials.htmlheader', ['headerTitle' => 'Consorcio AP'])
+@show
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    {!! Form::open(['method' => 'POST', 'route' => 'login', 'class' => 'form-horizontal']) !!}
-                        {!! Field::text('email', ['label' => 'Usuario', 'value' => old('user'), 'tpl' => 'templates/oneline']) !!}
-                        {!! Field::password('password', ['tpl' => 'templates/oneline']) !!}
+<body class="hold-transition login-page">
+        <div class="login-box">
 
-                        {!! Form::submit('Entrar', ['class' => 'btn btn-primary pull-right']) !!}
-                    {!! Form::close() !!}
+            <div class="login-box-body">
+
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8" align="center" valign="middle">
+                        <img width="100%" height="100%" src="{{ asset('/img/logocap.png') }}">
+                    </div>
                 </div>
+
+                {!! Form::open(['method' => 'POST', 'route' => 'login', 'class' => 'form-horizontal']) !!}
+
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                        {!! Field::text('email',
+                            ['label' => 'Usuario', 'value' => old('user'), 'tpl' => 'templates/withicon'],
+                            ['icon' => 'user-circle']) !!}
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                        {!! Field::password('password',
+                            ['tpl' => 'templates/withicon'], ['icon' => 'key']) !!}
+                    </div>
+                </div>
+
+                <br>
+
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
+                        {!! Form::submit('Entrar', ['class' => 'btn btn-warning btn-block']) !!}
+                    </div>
+                </div>
+                {!! Form::close() !!}
             </div>
         </div>
-    </div>
-</div>
-@endsection
+</body>
+
+</html>
