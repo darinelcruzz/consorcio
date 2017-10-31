e19f8cb9566928412ae58e6fa73c76a8b26b3ac8
Darinel <darinelcruzz@gmail.com>
2017-10-30 18:50:35 -0600

    Welcome message added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('admin')
 
 @section('main-content')
-<h2>Bienvenido.</h2>
+<h2>Bienvenido, {{ auth()->user()->name }}</h2>
     <div align="center">
     	<img width="40%" height="40%" src="{{ asset('/img/logocap.png') }}">
     </div>
