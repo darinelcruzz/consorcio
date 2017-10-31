262272bacc44d6ef0c1bc6f40b8884ff63077445
Darinel <darinelcruzz@gmail.com>
2017-10-30 20:49:08 -0600

    Routes clients and products moved to public

--- routes/public.php
+++ routes/public.php
@@ -15,3 +15,48 @@ Route::get('tests', function () {
     $sale = \App\ProcessedSale::find(1);
     return unserialize($sale->products);
 });
+
+Route::get('clients', function()
+{
+    $all = App\Client::all();
+
+    $clients = [];
+
+    foreach ($all as $client) {
+
+        $client = [
+            'id' => $client->id,
+            'name' => $client->name,
+            'address' => $client->address,
+            'notes' => $client->notes,
+            'unpaid' => $client->unpaid_notes,
+            'balance' => $client->balance,
+        ];
+
+        array_push($clients, (object) $client);
+
+    }
+
+    return $clients;
+});
+
+Route::get('products', function () {
+    $all = \App\Product::where('processed', 1)->get();
+
+    $products = [];
+    $price_id = '10';
+
+    foreach ($all as $p) {
+
+        $product = [
+            'id' => $p->id,
+            'name' => $p->name,
+            'price' => $p->price_alone
+        ];
+
+        array_push($products, (object) $product);
+
+    }
+
+    return $products;
+});
