2bb97196c69b87f51d775910daa765a73d6d92d6
Victor <victorjcg_6@hotmail.com>
2017-12-19 16:45:18 -0600

    delete all data in index

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -9,6 +9,7 @@
             <tr>
                 <th>ID</th>
                 <th>Nombre</th>
+                <th>R.F.C.</th>
                 <th>Dirección</th>
                 <th>Productos</th>
             </tr>
@@ -18,28 +19,7 @@
             @foreach($clients as $client)
                 <tr>
                     <td>{{ $client->id }}</td>
-                    <td>
-                        <a href="{{ route('client.details', ['id' => $client->id])}}">{{ $client->name }}</a> &nbsp;
-                        <a href="{{ route('client.edit', ['id' => $client->id])}}"
-                            title="EDITAR">
-                            <i class="fa fa-pencil" aria-hidden="true"></i>
-                        </a><br>
-                        {{ $client->email }} <br>
-                        {{ $client->phone }} <br>
-                        {{ $client->cellphone or ''}}
-                    </td>
-                    <td>{{ $client->address }}</td>
-                    <td>
-                        @foreach (unserialize($client->products) as $product)
-                            @if (!$loop->last)
-                                {{ $product }},
-                            @else
-                                {{ $product }}
-                            @endif
-                        @endforeach
-                        <br>
-                        Credito: {{ $client->credit == 1 ? 'Si Max: ' .  $client->notes  . ' Días ' . $client->days : 'No' }}
-                    </td>
+                    
                 </tr>
             @endforeach
         </template>
