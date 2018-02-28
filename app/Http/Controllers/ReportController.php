f9c7a6f2ad6073d9535ce3ead7a88e86de8e0310
Darinel Cruz <darinelcruzz@gmail.com>
2018-02-27 19:54:36 -0600

    Refactoring of ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -83,45 +83,26 @@ class ReportController extends Controller
             foreach ($sales as $sale) {
                 foreach (unserialize($sale->products) as $p) {
                     $kg = isset($p['k']) ? $p['k']: 0;
-                    $amount = $kg * $p['p'];
 
-                    switch ($p['i']) {
-                        case '4':
-                            array_push($big, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '5':
-                            array_push($medium, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '6':
-                            array_push($small, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '7':
-                            array_push($junior, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '8':
-                            array_push($petit, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '9':
-                            array_push($mini, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                    }
+                    $types = ['4' => 'big', '5' => 'medium', '6' => 'small', '7' => 'junior', '8' => 'petit', '9' => 'mini'];
+
+                    array_push(${$types[$p['i']]}, [
+                        'client' => $sale->client_id,
+                        'quantity' => $p['q'], 'kg' => $kg,
+                        'amount' => $kg * $p['p']
+                    ]);
                 }
             }
-            $bigC = collect($big);
-            $mediumC = collect($medium);
-            $smallC = collect($small);
-            $juniorC = collect($junior);
-            $petitC = collect($petit);
-            $miniC = collect($mini);
 
-            $bigG = $bigC->groupBy('client');
-            $mediumG = $mediumC->groupBy('client');
-            $smallG = $smallC->groupBy('client');
-            $juniorG = $juniorC->groupBy('client');
-            $petitG = $petitC->groupBy('client');
-            $miniG = $miniC->groupBy('client');
+            $data = [
+                'Grande' => collect($big)->groupBy('client'),
+                'Mediano' => collect($medium)->groupBy('client'),
+                'Chico' => collect($small)->groupBy('client'),
+                'Junior' => collect($junior)->groupBy('client'),
+                'Petit' => collect($petit)->groupBy('client'),
+                'Mini' => collect($mini)->groupBy('client')
+            ];
 
-            $data = ['Grande' => $bigG, 'Mediano' => $mediumG, 'Chico' => $smallG, 'Junior' => $juniorG, 'Petit' => $petitG, 'Mini' => $miniG];
             $product = 'Rangos';
 
             return view('reports.range', compact('product', 'data', 'range'));
@@ -133,62 +114,33 @@ class ReportController extends Controller
             foreach ($sales as $sale) {
                 foreach (unserialize($sale->products) as $p) {
                     $kg = isset($p['k']) ? $p['k']: 0;
-                    $amount = $kg * $p['p'];
 
-                    switch ($p['i']) {
-                        case '10':
-                            array_push($boneless, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '11':
-                            array_push($bone, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '12':
-                            array_push($foot, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '13':
-                            array_push($wings, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '14':
-                            array_push($wing, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '15':
-                            array_push($gizzard, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '16':
-                            array_push($visors, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                        case '17':
-                            array_push($pickled, ['client' => $sale->client_id, 'quantity' => $p['q'], 'kg' => $kg, 'amount' => $amount]);
-                            break;
-                    }
+                    $types = ['10' => 'boneless', '11' => 'bone', '12' => 'foot', '13' => 'wings', '14' => 'wing',
+                        '15' => 'gizzard', '16' => 'visors', '17' => 'pickled'];
+
+                    array_push(${$types[$p['i']]}, [ // ${$types[..]} devuelve $boneless, $bone...
+                        'client' => $sale->client_id,
+                        'quantity' => $p['q'], 'kg' => $kg,
+                        'amount' => $kg * $p['p']
+                    ]);
                 }
             }
-            $bonelessC = collect($boneless);
-            $boneC = collect($bone);
-            $footC = collect($foot);
-            $wingsC = collect($wings);
-            $wingC = collect($wing);
-            $gizzardC = collect($gizzard);
-            $visorsC = collect($visors);
-            $pickledC = collect($pickled);
 
-            $bonelessG = $bonelessC->groupBy('client');
-            $boneG = $boneC->groupBy('client');
-            $footG = $footC->groupBy('client');
-            $wingsG = $wingsC->groupBy('client');
-            $wingG = $wingC->groupBy('client');
-            $gizzardG = $gizzardC->groupBy('client');
-            $visorsG = $visorsC->groupBy('client');
-            $pickledG = $pickledC->groupBy('client');
+            $data = [
+                'Pechuga sin hueso' => collect($boneless)->groupBy('client'),
+                'Pechuga con hueso' => collect($bone)->groupBy('client'),
+                'Pierna y Muslo' => collect($foot)->groupBy('client'),
+                'Alas picosas' => collect($wings)->groupBy('client'),
+                'Ala 1 y 2' => collect($wing)->groupBy('client'),
+                'Molleja' => collect($gizzard)->groupBy('client'),
+                'Viscera mixta' => collect($visors)->groupBy('client'),
+                'Pollo Adobado' => collect($pickled)->groupBy('client')
+            ];
 
-            $data = ['Pechuga sin hueso' => $bonelessG, 'Pechuga con hueso' => $boneG, 'Pierna y Muslo' => $footG, 'Alas picosas' => $wingsG,
-            'Ala 1 y 2' => $wingG, 'Molleja' => $gizzardG, 'Viscera mixta' => $visorsG, 'Pollo Adobado' => $pickledG];
             $product = 'Cortes';
 
             return view('reports.curt', compact('product', 'data', 'range'));
         }
-
-
 
         return view('reports.product', compact('products', 'product', 'range'));
     }
