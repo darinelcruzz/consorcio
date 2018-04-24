59c12839a0ed099add83905e81a6202456a49b80
Darinel Cruz <darinelcruzz@gmail.com>
2018-04-24 12:17:23 -0500

    One more date added

--- app/Http/Composers/SalesComposer.php
+++ app/Http/Composers/SalesComposer.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\PorkSale;
 use Jenssegers\Date\Date;
 
-class PorkComposer
+class SalesComposer
 {
     public function compose(View $view)
     {
@@ -22,6 +22,7 @@ class PorkComposer
             Date::now()->format('Y-m-d') => Date::now()->format('l\, j F Y'),
             Date::now()->sub('1 day')->format('Y-m-d') => Date::now()->sub('1 day')->format('l\, j F Y'),
             Date::now()->sub('2 days')->format('Y-m-d') => Date::now()->sub('2 days')->format('l\, j F Y'),
+            Date::now()->sub('3 days')->format('Y-m-d') => Date::now()->sub('3 days')->format('l\, j F Y'),
         ];
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
 use Laravel\Dusk\DuskServiceProvider;
-use App\Http\Composers\PorkComposer;
+use App\Http\Composers\SalesComposer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -35,6 +35,6 @@ class AppServiceProvider extends ServiceProvider
 
     protected function registerViewComposers()
    {
-       View::composer('sales.*', PorkComposer::class);
+       View::composer('sales.*', SalesComposer::class);
    }
 }
