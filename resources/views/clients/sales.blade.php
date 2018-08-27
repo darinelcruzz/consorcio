f08cfae1f981b819c3a30e697c8ba8cb384ecb86
Darinel Cruz Vázquez <darinelcruzz@gmail.com>
2018-08-27 12:55:58 -0500

    Canceled sales not showing in client details

--- resources/views/clients/sales.blade.php
+++ resources/views/clients/sales.blade.php
@@ -16,27 +16,29 @@
 
     <template slot="body">
         @foreach($client->$sale as $sale)
-            <tr>
-                <td>{{ $sale->folio }}</td>
-                <td>{{ $sale->short_date }}</td>
-                <td>{{ $sale->kg }}</td>
-                <td>{{ $sale->pricer->name }}</td>
-                <td>{{ $sale->nice_amount }}</td>
-                <td>{{ $sale->credit == 0 ? 'Contado' : '$ ' . number_format($sale->amount - $sale->deposit_total, 2) }}</td>
-                <td>
-                    @includeWhen($sale->status != 'pagado' && auth()->user()->level == 1,'clients/deposit')
-                </td>
-                <td>{{ $sale->status }}</td>
-                @if ($sale->credit == 1)
+            @if ($sale->status != 'cancelada')
+                <tr>
+                    <td>{{ $sale->folio }}</td>
+                    <td>{{ $sale->short_date }}</td>
+                    <td>{{ $sale->kg }}</td>
+                    <td>{{ $sale->pricer->name }}</td>
+                    <td>{{ $sale->nice_amount }}</td>
+                    <td>{{ $sale->credit == 0 ? 'Contado' : '$ ' . number_format($sale->amount - $sale->deposit_total, 2) }}</td>
                     <td>
-                        <a href="{{ route('deposit.details', ['id' => $sale->id, 'type' => $sale->type, 'amount' => $sale->amount]) }}"  class="btn btn-info">
-                            <i class="fa fa-eye" aria-hidden="true"></i>
-                        </a>
+                        @includeWhen($sale->status != 'pagado' && auth()->user()->level == 1,'clients/deposit')
                     </td>
-                @else
-                    <td></td>
-                @endif
-            </tr>
+                    <td>{{ $sale->status }}</td>
+                    @if ($sale->credit == 1)
+                        <td>
+                            <a href="{{ route('deposit.details', ['id' => $sale->id, 'type' => $sale->type, 'amount' => $sale->amount]) }}"  class="btn btn-info">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+                            </a>
+                        </td>
+                    @else
+                        <td></td>
+                    @endif
+                </tr>
+            @endif
         @endforeach
     </template>
 
