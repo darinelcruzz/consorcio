93914374c71069183bf5a04b9c885ebd359b30f6
Darinel Cruz <darinelcruzz@gmail.com>
2021-05-19 16:18:33 -0500

    Problem with Translator...

--- app/Http/Composers/SalesComposer.php
+++ app/Http/Composers/SalesComposer.php
@@ -5,7 +5,7 @@ namespace App\Http\Composers;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Route;
 use App\{Price, Client};
-use Jenssegers\Date\Date;
+// use Jenssegers\Date\Date;
 
 class SalesComposer
 {
@@ -28,11 +28,11 @@ class SalesComposer
         $view->clients = Client::buyers($type);
 
         $view->validDates = [
-            Date::now()->format('Y-m-d') => Date::now()->format('l\, j F Y'),
-            Date::now()->sub('1 day')->format('Y-m-d') => Date::now()->sub('1 day')->format('l\, j F Y'),
-            Date::now()->sub('2 days')->format('Y-m-d') => Date::now()->sub('2 days')->format('l\, j F Y'),
-            Date::now()->sub('3 days')->format('Y-m-d') => Date::now()->sub('3 days')->format('l\, j F Y'),
-            Date::now()->sub('4 days')->format('Y-m-d') => Date::now()->sub('4 days')->format('l\, j F Y')
+            now()->format('Y-m-d') => now()->format('j F Y'),
+            now()->sub('1 day')->format('Y-m-d') => now()->sub('1 day')->format('j F Y'),
+            now()->sub('2 days')->format('Y-m-d') => now()->sub('2 days')->format('j F Y'),
+            now()->sub('3 days')->format('Y-m-d') => now()->sub('3 days')->format('j F Y'),
+            now()->sub('4 days')->format('Y-m-d') => now()->sub('4 days')->format('j F Y')
         ];
     }
 }
